62e4789d35687a5494225866eaec07dfdf09fdf9
myWebmasterUk <github@mywebmaster.co.uk>
2025-04-23 13:21:13 +0400

    Update supabase.js
    add supabaase paramns

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'YOUR_SUPABASE_URL';
-const supabaseKey = 'YOUR_SUPABASE_ANON_KEY';
+const supabaseUrl = 'https://lqmeshsxchiszfhpcmon.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxxbWVzaHN4Y2hpc3pmaHBjbW9uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyNjI4ODQsImV4cCI6MjA2MDgzODg4NH0.vbebcMK5CbEugUvYKZX7yWeBQ0Ip53q5XMoUfTMpqWQ';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
